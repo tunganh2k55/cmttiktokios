b763a5ec43a4cf859d1d2eae8b050d285b4130bf
tunganh2k55 <tunganhbackup911@gmail.com>
2026-01-19 06:25:08 +0700

    lên lớp code tiếp

--- module_regfb.py
+++ module_regfb.py
@@ -9,14 +9,63 @@ import random
 import subprocess
 
 
-class RegFb:
-    def get_ipv4_from_ipconfig():
-        output = subprocess.check_output("ipconfig", shell=True, text=True, encoding="utf-8", errors="ignore")
-        match = re.search(r"IPv4 Address[^\d]*([\d]+\.[\d]+\.[\d]+\.[\d]+)", output)
-        serverlocal = match.group(1) if match else None
-        return serverlocal
+class Proxy:
+    def getProxy(apikey):
+        api = f"https://api.xoayproxy.com/webservice/statusIP?key={apikey}"
+        try:
+            response = requests.get(api).json()
+            if response['status'] == 'OK':
+                proxy = response['data']['ipv4']
+                return proxy
+            elif response['status'] == 'Chưa đến thời gian đổi IP':
+                proxy = response['data']['ipv4']
+                return proxy
+        except Exception as e:
+            print(f"Error fetching proxy: {e}")
+            return None
 
-    def openApp(bundleID: str):
-        return f'at.appRun("{bundleID}")'
 
+    def changeProxy(serverlocal):
+        message = f"""
+            function findImage(image_path, rx, ry, rz, rt)
+                local startTime = os.time()
+                local timeout = 5
+                local region = {{rx, ry, rz, rt}}
+
+                while (os.time() - startTime) < timeout do
+                    local result = findImage(image_path, 1, 0.95, region, true, 1)
+
+                    if result ~= nil and #result > 0 then
+                        local x = result[1][1]
+                        local y = result[1][2]
+
+                        tap(x, y)
+                        usleep(1500000)
+                        return true, x, y
+                    end
+
+                    usleep(200000)
+                end
+
+                return false, nil, nil
+            end
+                    
+            appRun("com.liguangming.Shadowrocket")
+            usleep(5000000)
+
+            toast("OFF SHADOWROCKET")
+            local okClick, x,y = findImage("images/shadow_on.png", 570, 170, 200, 100)
+
+
+
+
+
+
+        """
+        return message
+
+
+
+proxy = Proxy.getProxy("IwucTpoAQae5e5REZojuqivAUSsJKtGS")
+print(proxy)
     
